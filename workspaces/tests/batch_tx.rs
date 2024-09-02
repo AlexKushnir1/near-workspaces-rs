2991573cc4f24ed5d6db928db46200c007e3a605
AlexKushnir1 <alex@alex-Precision-7510>
2024-09-02 14:19:23 +0300

    test ci

--- workspaces/tests/batch_tx.rs
+++ workspaces/tests/batch_tx.rs
@@ -22,7 +22,7 @@ async fn test_batch_tx() -> anyhow::Result<()> {
                 .deposit(NearToken::from_near(0)),
         )
         .call(Function::new("set_status").args_json(json!({
-            "message": "234325435",
+            "message": "world_hello",
         })))
         .transact()
         .await?
